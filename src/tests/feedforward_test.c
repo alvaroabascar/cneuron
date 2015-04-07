42f4de283624f51c5e04166bed72d919d1f75958
alvaro <alvaroabascar@gmail.com>
2015-04-07 17:37:53 +0200

    fixed weights

--- src/tests/feedforward_test.c
+++ src/tests/feedforward_test.c
@@ -9,10 +9,10 @@ int main(int argc, char *argv[])
   int net_structure[3] = {IN, HIDDEN, OUT};
   double input[IN] = {1, 2};
   double output[OUT];
-  struct network net = create_network(3, net_structure);
-  set_random_weights_biases(net);
+  struct network net = load_network("feedforward_test.net");
   feedforward(net, input, output);
   destroy_network(net);
+  printf("correct output: 0.7111\nactual output:\n");
   print_array_double(OUT, output);
   return 0;
 }
